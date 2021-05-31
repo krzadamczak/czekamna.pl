8aa4f2c50866ccae2edc54e9c6f9aac2508363f2
Krzysztof Adamczak <krzadamczak@gmail.com>
2021-05-31 15:24:19 +0200

    Zmiana portu serwera

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRoute);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Serwer działa');
 });
